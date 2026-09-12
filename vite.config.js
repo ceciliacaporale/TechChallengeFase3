70a023c9f80a8f7300521b6897cedbda3a2b269e
Don616 <contatodonilo@gmail.com>
2026-09-11 22:50:35 -0300

    add k8s

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,24 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  // Configuração para o modo preview (vite preview / npm start)
+  preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: [
+      'techchallenger.duckdns.org',
+      'localhost',
+      '127.0.0.1'
+    ]
+  },
+  // Configuração para o modo dev (vite dev)
+  server: {
+    host: true,
+    port: 3000,
+    allowedHosts: [
+      'techchallenger.duckdns.org',
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
 })
