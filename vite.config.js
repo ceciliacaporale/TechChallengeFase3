3e552e9659f9fedf839c4555f792917ecc84d5f4
Don616 <contatodonilo@gmail.com>
2026-09-11 22:59:05 -0300

    allow all host

--- vite.config.js
+++ vite.config.js
@@ -2,30 +2,14 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()])
   ],
-  // Configuração para o modo preview (vite preview / npm start)
   preview: {
     host: true,
     port: 3000,
-    allowedHosts: [
-      'techchallenger.duckdns.org',
-      'localhost',
-      '127.0.0.1'
-    ]
-  },
-  // Configuração para o modo dev (vite dev)
-  server: {
-    host: true,
-    port: 3000,
-    allowedHosts: [
-      'techchallenger.duckdns.org',
-      'localhost',
-      '127.0.0.1'
-    ]
+    allowedHosts: true // Libera o DuckDNS e qualquer Host header sem restrição
   }
 })
