c2132b9c48612da1c4fd43819a51b15510709f4a
Don616 <contatodonilo@gmail.com>
2026-09-11 23:24:03 -0300

    add /web

--- vite.config.js
+++ vite.config.js
@@ -5,25 +5,26 @@ import babel from '@rolldown/plugin-babel'
 // https://vite.dev/config/server-options#server-allowedhosts
 export default defineConfig({
   plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+react(),
+babel({ presets: [reactCompilerPreset()] })
+],
+  base: '/web/',
   preview: {
     host: true,
     port: 3000,
     allowedHosts: [
-      'techchallenger.duckdns.org',
-      'localhost',
-      '127.0.0.1'
-    ]
-  },
+'techchallenger.duckdns.org',
+'localhost',
+'127.0.0.1'
+]
+},
   server: {
     host: true,
     port: 3000,
     allowedHosts: [
-      'techchallenger.duckdns.org',
-      'localhost',
-      '127.0.0.1'
-    ]
-  }
+'techchallenger.duckdns.org',
+'localhost',
+'127.0.0.1'
+]
+}
 })
